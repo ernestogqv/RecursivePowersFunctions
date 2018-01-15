e7ded5fb90a3ab6935e663b1afe07c60beeedce2
ernestogqv <ernestogqv@gmail.com>
2018-01-14 22:03:52 -0500

    Update Main.java
    Another way of doing it with only one function but requires more parameters.

--- src/xkorpion/Main.java
+++ src/xkorpion/Main.java
@@ -1,19 +1,11 @@
 package xkorpion;
 
 public class Main {
-    public static int recursive2(int number, int power, int n, int counter){
+    public static int recursive(int number, int power, int n, int counter){
         n = n * number;
         counter++;
         if(n < power){
-            counter = recursive2(number, power, n, counter);
-        }
-        return counter;
-    }
-    public static int recursive(int number, int power){
-        int n = number * number;
-        int counter = 2;
-        if(n < power){
-            counter = recursive2(number, power, n, counter);
+            counter = recursive(number, power, n, counter);
         }
         return counter;
     }
